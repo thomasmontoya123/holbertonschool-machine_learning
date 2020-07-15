d117c043422a2a58fe8dd7a680d8b06aa47c8a91
Thomas <tomasmontoya123@gmail.com>
2020-07-15 16:23:48 -0500

    task 14 -- matrix multiplication using np

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Matrix multiplication module"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication"""
+    return np.dot(mat1, mat2)
