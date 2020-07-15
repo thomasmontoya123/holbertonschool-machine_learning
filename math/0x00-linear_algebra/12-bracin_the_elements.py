167fe2494214ec1fcd7fc4e7feb6a6649140512f
Thomas <tomasmontoya123@gmail.com>
2020-07-15 16:06:29 -0500

    task 12 -- matrix ops built in functions

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Oprations module"""
+
+
+def np_elementwise(mat1, mat2):
+    """Add. substract, mul, divide"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
