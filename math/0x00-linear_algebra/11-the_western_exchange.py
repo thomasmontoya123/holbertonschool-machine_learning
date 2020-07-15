adb6249d98059a90a874b1c608c7b88f17878e39
Thomas <tomasmontoya123@gmail.com>
2020-07-15 16:01:23 -0500

    task 11 -- matrix transpose built in function

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Matrix transpose module"""
+
+
+def np_transpose(matrix):
+    """Transposes matrix using built in function"""
+    transposed = matrix.transpose()
+
+    return transposed
