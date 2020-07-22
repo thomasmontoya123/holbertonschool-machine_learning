6ab4f7793bee629ab372d14ed8e7a09158373010
Thomas <tomasmontoya123@gmail.com>
2020-07-21 20:32:33 -0500

    task 4 -- Histogram

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
+plt.title("Project A")
+plt.xlim(0, 100)
+plt.xticks(np.arange(0, 110, step=10))
+plt.ylim(0, 30)
+plt.plot()
+plt.show()
