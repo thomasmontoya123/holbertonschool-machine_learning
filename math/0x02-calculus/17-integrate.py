7ec5ef22f5129c552ab904ee1d3bb0cc64519bdb
Thomas <tomasmontoya123@gmail.com>
2020-07-26 18:58:18 -0500

    task 17  -- calculates the integral of a polynomial

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -9,6 +9,9 @@ def poly_integral(poly, C=0):
             or type(C) not in [int, float]:
         return None
 
+    if poly == [0]:
+        return [C]
+
     result = [poly[i] / (i + 1) for i in range(len(poly))]
     result.insert(0, C)
     cleaned = [int(i) if i % 1 == 0 else i for i in result]
