576d0885a57b7e5e4650ca3e8135237b41b35287
Thomas <tomasmontoya123@gmail.com>
2020-07-30 16:22:18 -0500

    Task 1 -- Poisson PMF

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -4,6 +4,7 @@
 
 class Poisson(object):
     """Represents a poisson distribution"""
+    e = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
         """Constructor"""
@@ -20,3 +21,16 @@ class Poisson(object):
 
             else:
                 self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of successes"""
+        if k < 0:
+            return 0
+
+        k = int(k)
+        fact = 1
+        for i in range(1, k + 1):
+            fact = fact * i
+
+        pmf = ((self.e ** (- self.lambtha)) * (self.lambtha ** k)) / fact
+        return pmf
