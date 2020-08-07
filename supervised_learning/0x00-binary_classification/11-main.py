1b0c9e5b7bd2280a29f8e43a4017872715e8a1dd
Thomas <tomasmontoya123@gmail.com>
2020-08-07 18:10:11 -0500

    Task 11 -- NeuralNetwork Cost

---
+++ supervised_learning/0x00-binary_classification/11-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+NN = __import__('11-neural_network').NeuralNetwork
+
+lib_train = np.load('/Users/thomas/PycharmProjects/holbertonschool-machine_learning/supervised_learning/0x00'
+                    '-binary_classification/data/Binary_Train.npz')
+X_3D, Y = lib_train['X'], lib_train['Y']
+X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+np.random.seed(0)
+nn = NN(X.shape[0], 3)
+_, A = nn.forward_prop(X)
+cost = nn.cost(Y, A)
+print(cost)
