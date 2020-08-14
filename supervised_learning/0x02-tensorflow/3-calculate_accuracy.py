1f6fb34858baeb20e850f0624ac7ed878a71b188
Thomas <tomasmontoya123@gmail.com>
2020-08-13 19:05:32 -0500

    task 3 -- Accuracy

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -18,5 +18,4 @@ def calculate_accuracy(y, y_pred):
     prediction_y_pred = tf.argmax(y_pred, axis=1)
     equality = tf.equal(prediction_y, prediction_y_pred)
     accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
-    sess = tf.Session()
     return accuracy
