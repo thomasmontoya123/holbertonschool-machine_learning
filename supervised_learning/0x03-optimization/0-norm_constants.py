7d5e692501b45fc5a3ae4e911cd66bd5de9d9c1e
Thomas <tomasmontoya123@gmail.com>
2020-08-17 15:49:20 -0500

    Task 0 -- Normalization Constants

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Constant normalization module"""
+
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+        calculates the normalization (standardization)
+        constants of a matrix
+            Parameters
+            ----------
+            X : numpy.ndarray
+                shape (m, nx) to normalize
+                    m is the number of data points
+                    nx is the number of features
+    """
+    return np.mean(X, axis=0), np.std(X, axis=0)
