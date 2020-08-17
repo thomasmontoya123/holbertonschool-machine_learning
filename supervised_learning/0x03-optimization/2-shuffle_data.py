3b8d8446e5e8384d62fe46aacfdb48e5571095f4
Thomas <tomasmontoya123@gmail.com>
2020-08-17 16:16:56 -0500

    Task 2 -- Shuffle Data

---
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+"""Normalization module"""
+
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """
+        Normalizes (standardizes) a matrix
+                Parameters
+                ----------
+                X : numpy.ndarray
+                    shape (m, nx) to normalize
+                        m is the number of data points
+                        nx is the number of features in X
+                Y : numpy.ndarray
+                    shape (m, ny) to normalize
+                        m is the number of data points
+                        ny is the number of features in Y
+    """
+    indices = np.random.permutation(X.shape[0])
+    return X[indices], Y[indices]
