3e7bb0f4bcc000c50f28eee18a883b5db8b893fb
Thomas <tomasmontoya123@gmail.com>
2020-08-23 15:10:58 -0500

    Task 3 -- Mini-Batch -- load_model() rmeove -- loads the model inside the original function

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -7,26 +7,6 @@ import numpy as np
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def mode_loader(path):
-    """
-        model load
-            Parameters
-            ----------
-            path : str
-                model to load
-    """
-    sess = tf.Session()
-
-    saver = tf.train.import_meta_graph(path + ".meta")
-    saver.restore(sess, path)
-    x = tf.get_collection("x")[0]
-    y = tf.get_collection("y")[0]
-    accuracy = tf.get_collection("accuracy")[0]
-    loss = tf.get_collection("loss")[0]
-    train_op = tf.get_collection("train_op")[0]
-    return x, y, accuracy, loss, train_op, saver, sess
-
-
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
                      epochs=5, load_path="/tmp/model.ckpt",
                      save_path="/tmp/model.ckpt"):
@@ -62,14 +42,19 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
             save_path : str
                 path to where the model should be saved after training
     """
-    x, y, accuracy, loss, train_op, saver, sess = mode_loader(load_path)
-
     m = X_train.shape[0]
     batch_len = m // batch_size
     if m % batch_size != 0:
         batch_len += 1
 
-    with sess:
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(load_path + ".meta")
+        saver.restore(sess, load_path)
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+        train_op = tf.get_collection("train_op")[0]
         for epoch in range(epochs + 1):
             train_cost, train_accuracy = sess.run([loss, accuracy],
                                                   feed_dict={x: X_train,
