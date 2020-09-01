8d39f5dd233b7370d81f6642c042a38faae887c4
Thomas <tomasmontoya123@gmail.com>
2020-08-31 19:53:29 -0500

    [Add] task 5 -- Train with validation data

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent
+        :param network: model to train
+        :param data: numpy.ndarray of shape (m, nx) containing the input data
+        :param labels: one-hot numpy.ndarray of shape (m, classes)
+            containing the labels of data
+        :param batch_size: size of the batch used for
+            mini-batch gradient descent
+        :param epochs: number of passes through data for mini-batch
+            gradient descent
+        :param verbose: boolean that determines if output should be printed
+            during training
+        :param shuffle: boolean that determines whether to shuffle the
+            batches every epoch
+        :param validation_data : data to validate the model with
+    """
+    return network.fit(x=data,
+                       y=labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data)
