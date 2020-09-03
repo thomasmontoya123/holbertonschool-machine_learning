[Fix] Task 1 -- Paddding formula

math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -16,8 +16,8 @@ def convolve_grayscale_same(images, kernel):
     input_w, input_h = images.shape[2], images.shape[1]
     kernel_w, kernel_h = kernel.shape[1], kernel.shape[0]
 
-    pad_w = int(kernel_w - 1 / 2)
-    pad_h = int(kernel_h - 1 / 2)
+    pad_w = int(kernel_w / 2)
+    pad_h = int(kernel_h / 2)
 
     in_padded = np.pad(array=images,
                        pad_width=((0, 0), (pad_h, pad_h), (pad_w, pad_w)),