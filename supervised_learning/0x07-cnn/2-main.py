[Fix] Task 2 -- Cnn Backpropagation -- ph and pw calculated using ceil

supervised_learning/0x07-cnn/2-main.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     b = np.random.randn(1, 1, 1, 2)
 
     dZ = np.random.randn(10, h - 2, w - 2, 2)
-    print(conv_backward(dZ, X_train_c, W, b, padding="valid"))
+    print(conv_backward(dZ, X_train_c, W, b, padding="same"))